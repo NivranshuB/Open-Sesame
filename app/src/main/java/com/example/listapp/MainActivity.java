074d726b74f1b43e3270450b9172dd75ff03ce13
Corban Draper <cdra334@aucklanduni.ac.nz>
2021-09-28 17:03:30 +1300

    feat added ListActivity initial GUI design

--- app/src/main/java/com/example/listapp/MainActivity.java
+++ app/src/main/java/com/example/listapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.listapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relative_layout_wooden);
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent woodenIntent = new Intent(getBaseContext(), ListActivity.class);
+                startActivity(woodenIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return true;
     }
 }
