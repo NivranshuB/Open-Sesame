de674c3657968e3874cdce94b649e20dd46ece49
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-10-04 16:50:20 +1300

    Add implementation to override the getMaterial() method in the Door subclasses.

--- app/src/main/java/com/example/listapp/model/MetalDoor.java
+++ app/src/main/java/com/example/listapp/model/MetalDoor.java
@@ -1,4 +1,18 @@
 package com.example.listapp.model;
 
 public class MetalDoor extends Door {
+
+    String materialImage;
+
+    /**
+     * @return the full qualified name of the material image of this Item instance
+     */
+    @Override
+    public String getMaterialImage() {
+        return null;
+    }
+
+    public void setMaterialImage(String newMaterialImage) {
+        materialImage = newMaterialImage;
+    }
 }
