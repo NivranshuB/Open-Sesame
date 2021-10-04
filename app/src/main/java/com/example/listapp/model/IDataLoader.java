7e23aefcb42d93ca396516f322dcea7fcbeae3ae
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-10-04 15:16:50 +1300

    Added method signatures for the methods of the interface IDataLoader.

--- app/src/main/java/com/example/listapp/model/IDataLoader.java
+++ app/src/main/java/com/example/listapp/model/IDataLoader.java
@@ -1,4 +1,19 @@
 package com.example.listapp.model;
 
+import java.util.List;
+
 public interface IDataLoader {
+
+    public void initialiseData();
+
+    public List<Item> getItemsByString(String searchString);
+
+    public List<Item> getItemsByCriteria(String categoryName);
+
+    public Item getItemByName(String itemName);
+
+    public List<Item> sortItemListByViewCount();
+
+    public void persistData();
+
 }
