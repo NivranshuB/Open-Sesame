c0e71acba1493c21a2708ef8509583b15b5fc46c
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-10-04 15:08:00 +1300

    Added the empty ItemAdapter class under a new package called 'adapters' in the listapp package.

---
+++ app/src/main/java/com/example/listapp/adapters/ItemAdapter.java
@@ -0,0 +1,12 @@
+package com.example.listapp.adapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+public class ItemAdapter extends ArrayAdapter {
+    public ItemAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+}
