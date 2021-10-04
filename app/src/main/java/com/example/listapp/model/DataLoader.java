1f58f42aacbdfed29957b44cc4066e86cc92b174
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-10-04 16:56:35 +1300

    Added documentation and empty methods to the DataLoader class which implements IDataLoader interface.

--- app/src/main/java/com/example/listapp/model/DataLoader.java
+++ app/src/main/java/com/example/listapp/model/DataLoader.java
@@ -1,4 +1,47 @@
 package com.example.listapp.model;
 
+import java.util.List;
+
+/**
+ * Open Sesame
+ *
+ * This class implements the responsibilities as specified by the IDataLoader interface. This class
+ * performs data retrievals and also data persistence to and from the application's firestore
+ * database.
+ */
 public class DataLoader implements IDataLoader {
+
+    @Override
+    public void initialiseData() {
+        //todo
+    }
+
+    @Override
+    public List<Item> getItemsByString(String matchString) {
+        //todo
+        return null;
+    }
+
+    @Override
+    public List<Item> getItemsByCriteria(String categoryName) {
+        //todo
+        return null;
+    }
+
+    @Override
+    public Item getItemByName(String itemName) {
+        //todo
+        return null;
+    }
+
+    @Override
+    public List<Item> sortItemListByViewCount() {
+        //todo
+        return null;
+    }
+
+    @Override
+    public void persistData(Item itemChanged) {
+        //todo
+    }
 }
