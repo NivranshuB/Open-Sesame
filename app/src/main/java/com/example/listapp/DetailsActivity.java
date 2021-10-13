f7012727fd41bf472228aa2f1293c70520216bb1
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-10-13 13:26:10 +1300

    FEAT - Added functionality in details activity such that it displays the name, description and price of an image.

--- app/src/main/java/com/example/listapp/DetailsActivity.java
+++ app/src/main/java/com/example/listapp/DetailsActivity.java
@@ -5,6 +5,16 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.ViewCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.listapp.model.Door;
+import com.example.listapp.model.Item;
+import com.example.listapp.model.WoodenDoor;
+
+import java.util.ArrayList;
+import java.util.List;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,6 +22,25 @@ import android.widget.RelativeLayout;
 import android.util.Log;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private class ViewHolder {
+        //The views in details activity go here
+        TextView itemName;
+        TextView description;
+        TextView itemSpecification;
+        TextView price;
+
+        public ViewHolder() {
+            //The elements common among all items assigned here
+            itemName = findViewById(R.id.item_name);
+            description = findViewById(R.id.item_description_text);
+            itemSpecification = findViewById(R.id.item_specification_text);
+            price = findViewById(R.id.item_price);
+        }
+    }
+
+    Item defaultItem;
+    Item itemSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +52,47 @@ public class DetailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.custom_toolbar_details);
         setSupportActionBar(toolbar);
 
+        ViewHolder detailsActivityVh = new ViewHolder();
+
+        createDefaultItem();
+
+        String nameString = "";
+
+        for (String s : defaultItem.getName()) {
+            nameString += " " + s;
+        }
+
+        detailsActivityVh.itemName.setText(nameString);
+        detailsActivityVh.description.setText(defaultItem.getDescription());
+        detailsActivityVh.price.setText("$" + defaultItem.getPrice());
+
+    }
+
+    private void createDefaultItem() {
+        List<Integer> dimensions = new ArrayList<>();
+        dimensions.add(Integer.valueOf(2355));
+        dimensions.add(Integer.valueOf(566));
+        dimensions.add(Integer.valueOf(36));
+
+        List<String> name = new ArrayList<>();
+        name.add("Authentic");
+        name.add("Gloss");
+        name.add("Finished");
+        name.add("Timber");
+        name.add("Door");
+
+        List<String> colour = new ArrayList<>();
+        colour.add("#000000");
+        colour.add("#000000");
+        colour.add("#000000");
+
+        List<String> images = new ArrayList<>();
+        images.add("handle3_1");
+        images.add("handle3_2");
+        images.add("handle3_3");
+
+        defaultItem = new WoodenDoor(1, 560, 43, 50.50f, dimensions, name,
+                "dsafk sadflkd dslfka dsa", colour, images);
         RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.description_relative_layout);
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
         relativeLayout.startAnimation(animation);
