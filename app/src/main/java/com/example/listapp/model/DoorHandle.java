a12ce1eb7b80a9df5246ec3893327f72ea9aef2c
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-10-22 12:39:06 +1300

    TEST - Added an explicit constructor in DoorHandle for testing purposes.

--- app/src/main/java/com/example/listapp/model/DoorHandle.java
+++ app/src/main/java/com/example/listapp/model/DoorHandle.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class DoorHandle extends Handle {
 
-
     @Override
     public List<String> getImage() {
         return image;
@@ -13,5 +12,20 @@ public class DoorHandle extends Handle {
     @Override
     public List<Long> getDimensions() {
         return dimensions;
+    }
+
+    public DoorHandle(int id, int weight, int viewCount, float price,
+                     List<Long> dimensions, List<String> name, String description,
+                     List<String> colour, List<String> image, Boolean lockable) {
+        this.id = id;
+        this.weight = weight;
+        this.viewCount = viewCount;
+        this.price = price;
+        this.dimensions = dimensions;
+        this.name = name;
+        this.description = description;
+        this.colour = colour;
+        this.image = image;
+        this.lockable = lockable;
     }
 }
