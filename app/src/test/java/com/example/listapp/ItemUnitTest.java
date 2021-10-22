6e216e4b4a209ac1b66b240cd41e81e043289ae5
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-10-22 13:11:44 +1300

    TEST - Added test cases in ItemUnitTest which test the reference fields (and corresponding getter methods) of all four item types after they have been constructed.

--- app/src/test/java/com/example/listapp/ItemUnitTest.java
+++ app/src/test/java/com/example/listapp/ItemUnitTest.java
@@ -69,9 +69,9 @@ public class ItemUnitTest {
         glassDoorImages.add("door14_3");
 
         List<String> doorHandleImages = new ArrayList<>();
-        glassDoorImages.add("handle11_1");
-        glassDoorImages.add("handle11_2");
-        glassDoorImages.add("handle11_3");
+        doorHandleImages.add("handle11_1");
+        doorHandleImages.add("handle11_2");
+        doorHandleImages.add("handle11_3");
 
         String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum";
 
@@ -84,13 +84,13 @@ public class ItemUnitTest {
         glassDoor = new GlassDoor(3, 100, 170, 30.05f, dimensions,
                 nameGlassDoor, description, colour, glassDoorImages);
 
-        doorHandle = new DoorHandle(4, 30, 390, 80.60f, dimensions,
+        doorHandle = new DoorHandle(4, 30, 390, 80.60f, null,
                 nameDoorHandle, description, colour, doorHandleImages, false);
     }
 
     /**
-     * Check the construction of a WoodenDoor instance and if it has all the relevant attributes
-     * that an Item must have.
+     * Check the construction of a WoodenDoor instance and if it has all the relevant primitive
+     * attributes that an Item must have.
      */
     @Test
     public void TestWoodenDoorPrimitives() {
@@ -103,7 +103,8 @@ public class ItemUnitTest {
     }
 
     /**
-     *
+     * Check the construction of a WoodenDoor instance and if the sizes of its list attributes are
+     * as to be expected.
      */
     @Test
     public void TestWoodenDoorReferenceSizes() {
@@ -113,24 +114,43 @@ public class ItemUnitTest {
     }
 
     /**
-     * Check the construction of a WoodenDoor instance and if it has all the relevant attributes
-     * that an Item must have.
+     * Check the construction of a WoodenDoor instance and if it has all the relevant reference
+     * (list) attributes that an Item must have.
      */
     @Test
     public void TestWoodenDoorReferences() {
+        List<String> nameWoodenDoor = new ArrayList<>();
+        nameWoodenDoor.add("Large");
+        nameWoodenDoor.add("Timber");
+        nameWoodenDoor.add("Wooden");
+        nameWoodenDoor.add("Door");
+
+        for (int i = 0; i < woodenDoor.getName().size(); i++) {
+            assertEquals(nameWoodenDoor.get(i), woodenDoor.getName().get(i));
+        }
+
         List<Long> dimensions = new ArrayList<>();
         dimensions.add(1600L);
         dimensions.add(600L);
         dimensions.add(36L);
 
         for (int i = 0; i < woodenDoor.getDimensions().size(); i++) {
-            assertEquals(woodenDoor.getDimensions().get(i), dimensions.get(i));
+            assertEquals(dimensions.get(i), woodenDoor.getDimensions().get(i));
+        }
+
+        List<String> woodenDoorImages = new ArrayList<>();
+        woodenDoorImages.add("door21_1");
+        woodenDoorImages.add("door21_2");
+        woodenDoorImages.add("door21_3");
+
+        for (int i = 0; i < woodenDoor.getImage().size(); i++) {
+            assertEquals(woodenDoorImages.get(i), woodenDoor.getImage().get(i));
         }
     }
 
     /**
-     * Check the construction of a MetalDoor instance and if it has all the relevant attributes
-     * that an Item must have.
+     * Check the construction of a MetalDoor instance and if it has all the relevant primitive
+     * attributes that an Item must have.
      */
     @Test
     public void TestMetalDoorPrimitives() {
@@ -143,18 +163,53 @@ public class ItemUnitTest {
     }
 
     /**
-     *
+     * Check the construction of a MetalDoor instance and if the sizes of its list attributes are
+     * as to be expected.
      */
     @Test
     public void TestMetalDoorReferenceSizes() {
-        assertEquals(metalDoor.getDimensions().size(), 3);
-        assertEquals(metalDoor.getName().size(), 3);
-        assertEquals(metalDoor. getImage().size(), 3);
+        assertEquals(3, metalDoor.getDimensions().size());
+        assertEquals(3, metalDoor.getName().size());
+        assertEquals(3, metalDoor.getImage().size());
     }
 
     /**
-     * Check the construction of a GlassDoor instance and if it has all the relevant attributes
-     * that an Item must have.
+     * Check the construction of a MetalDoor instance and if it has all the relevant reference
+     * (list) attributes that an Item must have.
+     */
+    @Test
+    public void TestMetalDoorReferences() {
+        List<String> nameMetalDoor = new ArrayList<>();
+        nameMetalDoor.add("Thin");
+        nameMetalDoor.add("Metal");
+        nameMetalDoor.add("Door");
+
+        for (int i = 0; i < metalDoor.getName().size(); i++) {
+            assertEquals(nameMetalDoor.get(i), metalDoor.getName().get(i));
+        }
+
+        List<Long> dimensions = new ArrayList<>();
+        dimensions.add(1600L);
+        dimensions.add(600L);
+        dimensions.add(36L);
+
+        for (int i = 0; i < metalDoor.getDimensions().size(); i++) {
+            assertEquals(dimensions.get(i), metalDoor.getDimensions().get(i));
+        }
+
+        List<String> metalDoorImages = new ArrayList<>();
+        metalDoorImages.add("door27_1");
+        metalDoorImages.add("door27_2");
+        metalDoorImages.add("door27_3");
+
+        for (int i = 0; i < metalDoor.getImage().size(); i++) {
+            assertEquals(metalDoorImages.get(i), metalDoor.getImage().get(i));
+        }
+    }
+
+    /**
+     * Check the construction of a GlassDoor instance and if it has all the relevant primitive
+     * attributes that an Item must have.
      */
     @Test
     public void TestGlassDoorPrimitives() {
@@ -167,18 +222,54 @@ public class ItemUnitTest {
     }
 
     /**
-     *
+     * Check the construction of a GlassDoor instance and if the sizes of its list attributes are
+     * as to be expected.
      */
     @Test
     public void TestGlassDoorReferenceSizes() {
-        assertEquals(glassDoor.getDimensions().size(), 3);
-        assertEquals(glassDoor.getName().size(), 4);
-        assertEquals(glassDoor. getImage().size(), 3);
+        assertEquals(3, glassDoor.getDimensions().size());
+        assertEquals(4, glassDoor.getName().size());
+        assertEquals(3, glassDoor.getImage().size());
     }
 
     /**
-     * Check the construction of a DoorHandle instance and if it has all the relevant attributes
-     * that an Item must have.
+     * Check the construction of a GlassDoor instance and if it has all the relevant reference
+     * (list) attributes that an Item must have.
+     */
+    @Test
+    public void TestGlassDoorReferences() {
+        List<String> nameGlassDoor = new ArrayList<>();
+        nameGlassDoor.add("Strong");
+        nameGlassDoor.add("Blue");
+        nameGlassDoor.add("Glass");
+        nameGlassDoor.add("Door");
+
+        for (int i = 0; i < glassDoor.getName().size(); i++) {
+            assertEquals(nameGlassDoor.get(i), glassDoor.getName().get(i));
+        }
+
+        List<Long> dimensions = new ArrayList<>();
+        dimensions.add(1600L);
+        dimensions.add(600L);
+        dimensions.add(36L);
+
+        for (int i = 0; i < glassDoor.getDimensions().size(); i++) {
+            assertEquals(dimensions.get(i), glassDoor.getDimensions().get(i));
+        }
+
+        List<String> glassDoorImages = new ArrayList<>();
+        glassDoorImages.add("door14_1");
+        glassDoorImages.add("door14_2");
+        glassDoorImages.add("door14_3");
+
+        for (int i = 0; i < glassDoor.getImage().size(); i++) {
+            assertEquals(glassDoorImages.get(i), glassDoor.getImage().get(i));
+        }
+    }
+
+    /**
+     * Check the construction of a DoorHandle instance and if it has all the relevant primitive
+     * attributes that an Item must have.
      */
     @Test
     public void TestHandlePrimitives() {
@@ -188,6 +279,44 @@ public class ItemUnitTest {
         assertEquals(80.60f, doorHandle.getPrice(),1);
         assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum",
                 doorHandle.getDescription());
+    }
+
+    /**
+     * Check the construction of a DoorHandle instance and if the sizes of its list attributes are
+     * as to be expected. Note the dimensions of a door handle will be null since this attribute
+     * does not apply to door handles.
+     */
+    @Test
+    public void TestHandleReferenceSizes() {
+        assertNull(doorHandle.getDimensions());
+        assertEquals(4, doorHandle.getName().size());
+        assertEquals(3, doorHandle.getImage().size());
+    }
+
+    /**
+     * Check the construction of a DoorHandle instance and if it has all the relevant reference
+     * (list) attributes that an Item must have barring the dimensions attribute.
+     */
+    @Test
+    public void TestHandleReferences() {
+        List<String> nameDoorHandle = new ArrayList<>();
+        nameDoorHandle.add("Sleek");
+        nameDoorHandle.add("Golden");
+        nameDoorHandle.add("Door");
+        nameDoorHandle.add("Handle");
+
+        for (int i = 0; i < doorHandle.getName().size(); i++) {
+            assertEquals(nameDoorHandle.get(i), doorHandle.getName().get(i));
+        }
+
+        List<String> doorHandleImages = new ArrayList<>();
+        doorHandleImages.add("handle11_1");
+        doorHandleImages.add("handle11_2");
+        doorHandleImages.add("handle11_3");
+
+        for (int i = 0; i < doorHandle.getImage().size(); i++) {
+            assertEquals(doorHandleImages.get(i), doorHandle.getImage().get(i));
+        }
     }
 
     /**
