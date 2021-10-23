ec2825a967923f97278d15e6e8de35a430fe3993
Corban Draper <cdra334@aucklanduni.ac.nz>
2021-10-24 00:00:39 +1300

    chore refactored MainActivity separated code into different methods to promote single responsibility removed redundant code

--- app/src/main/java/com/example/listapp/MainActivity.java
+++ app/src/main/java/com/example/listapp/MainActivity.java
@@ -50,7 +50,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements PanelViewAdapter.OnItemClickListener, ItemAdapter.OnItemClickListener{
+public class MainActivity extends AppCompatActivity implements PanelViewAdapter.OnItemClickListener, ItemAdapter.OnItemClickListener {
 
     private class ViewHolder {
         //The views in main activity go here
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity implements PanelViewAdapter.
         RelativeLayout handle_category_button;
         BottomNavigationView bottomNavigationView;
         RecyclerView panel_recycler_view;
+        Toolbar toolbar;
 
         public ViewHolder() {
             //The elements common among all items assigned here
@@ -69,6 +70,7 @@ public class MainActivity extends AppCompatActivity implements PanelViewAdapter.
             handle_category_button = findViewById(R.id.relative_layout_handles);
             panel_recycler_view = findViewById(R.id.panelRecyclerView);
             bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
+            toolbar = findViewById(R.id.custom_toolbar);
         }
     }
 
@@ -84,116 +86,23 @@ public class MainActivity extends AppCompatActivity implements PanelViewAdapter.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("On creation");
-//        initPanelItems();
-        Log.d("afterInit", "On creation after initPanelItems()");
 
         mainActivityVH = new ViewHolder();
 
-        mainActivityVH.panel_recycler_view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                if (panelViewDone) {
-                    mainActivityVH.panel_recycler_view.getViewTreeObserver().removeOnPreDrawListener(this);
-
-                    Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
-                    Animation panelViewAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.panel_view_slide_from_right);
-                    mainActivityVH.wooden_category_button.startAnimation(animation);
-                    mainActivityVH.metal_category_button.startAnimation(animation);
-                    mainActivityVH.glass_category_button.startAnimation(animation);
-                    mainActivityVH.handle_category_button.startAnimation(animation);
-                    findViewById(R.id.panelRecyclerView).startAnimation(panelViewAnimation);
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
+        startAnimation();
 
         initPanelRecyclerView();
 
-        mainActivityVH.wooden_category_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
-                listActivity.putExtra("type", "wooden");
-                startActivity(listActivity);
-//                ActivityOptionsCompat activityOptions = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this,
-//                        findViewById(R.id.custom_toolbar), "listActivityTransition"); //new Pair<>(view, "topPicksImageTransition")
-//
-//                ActivityCompat.startActivity(MainActivity.this, listActivity, activityOptions.toBundle());
-                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-            }
-        });
+        createCategoryClickListeners();
 
-        mainActivityVH.metal_category_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
-                listActivity.putExtra("type", "metallic");
-                startActivity(listActivity);
-                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-            }
-        });
+        setSupportActionBar(mainActivityVH.toolbar);
 
-        mainActivityVH.glass_category_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
-                listActivity.putExtra("type", "glass");
-                startActivity(listActivity);
-                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-            }
-        });
+        initBottomNavigationView();
 
-        mainActivityVH.handle_category_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
-                listActivity.putExtra("type", "handle");
-                startActivity(listActivity);
-                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-            }
-        });
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.custom_toolbar);
-        setSupportActionBar(toolbar);
-
-
-        mainActivityVH.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                if (item.getItemId() == R.id.home_page) {
-
-                    return true;
-                } else if (item.getItemId() == R.id.favourites_page) {
-                    Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
-                    listActivity.putExtra("type", "favourites");
-                    startActivity(listActivity);
-                    overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-                    return true;
-                }
-                return false;
-            }
-        });
-
-//        CardView cardView = (CardView) findViewById(R.id.card_view_1);
-//        cardView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                System.out.println("Details activity clicked");
-//                Intent detailIntent = new Intent(getBaseContext(), DetailsActivity.class);
-//                ActivityOptionsCompat activityOptions = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this,
-//                        new Pair<>(view.findViewById(R.id.image_1), "topPicksImageTransition"));
-//                ActivityCompat.startActivity(MainActivity.this, detailIntent, activityOptions.toBundle());
-////                startActivity(detailIntent);
-//            }
-//        });
     }
+
     @Override
-    public void onResume()
-    {  // After a pause OR at startup
+    public void onResume() {  // After a pause OR at startup
         super.onResume();
         //Refresh your stuff here
         mainActivityVH.bottomNavigationView.setSelectedItemId(R.id.home_page);
@@ -229,42 +138,31 @@ public class MainActivity extends AppCompatActivity implements PanelViewAdapter.
         return true;
     }
 
-    private void initPanelItems() {
-        // TODO: 05/10/2021 Requires implementation of code to retrieve data from Firestore DB and helper functions to sort and etc, in order to populate panelItems list.
-        List<Integer> temp = new ArrayList<>();
-        temp.add(5);
-        temp.add(17);
-        temp.add(12);
-        temp.add(37);
-        temp.add(24);
-
-        dataLoader.getItemsByID(temp, new DataCallback() {
+    private void startAnimation() {
+        mainActivityVH.panel_recycler_view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
             @Override
-            public void dataListCallback(List<Item> itemlist) {
-                for (Item i : itemlist) {
-                    Log.d("testname", i.getName().toString());
-                    Log.d("testid", Integer.toString(i.getId()));
-                    Log.d("testViewCount", Integer.toString(i.getViewCount()));
-                    Log.d("testFirestoreID", i.getFirestoreID());
-                    dataLoader.persistData(i);
+            public boolean onPreDraw() {
+                if (panelViewDone) {
+                    mainActivityVH.panel_recycler_view.getViewTreeObserver().removeOnPreDrawListener(this);
+
+                    Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
+                    Animation panelViewAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.panel_view_slide_from_right);
+                    mainActivityVH.wooden_category_button.startAnimation(animation);
+                    mainActivityVH.metal_category_button.startAnimation(animation);
+                    mainActivityVH.glass_category_button.startAnimation(animation);
+                    mainActivityVH.handle_category_button.startAnimation(animation);
+                    mainActivityVH.panel_recycler_view.startAnimation(panelViewAnimation);
+                    return true;
+                } else {
+                    return false;
                 }
             }
-
-            @Override
-            public void itemCallback(Item i) {
-
-            }
         });
-        Log.d("initialise", "initPanelItems running");
-
     }
 
 
     private void initPanelRecyclerView() {
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        mainActivityVH.panel_recycler_view.setLayoutManager(linearLayoutManager);
-
-        DataLoader dataLoader = new DataLoader();
+        dataLoader = new DataLoader();
         dataLoader.sortItemListByViewCount(new DataCallback() {
             @Override
             public void dataListCallback(List<Item> itemList) {
@@ -280,61 +178,74 @@ public class MainActivity extends AppCompatActivity implements PanelViewAdapter.
         });
     }
 
+    private void initBottomNavigationView() {
+        mainActivityVH.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.home_page) {
+                    return true;
+                } else if (item.getItemId() == R.id.favourites_page) {
+                    Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
+                    listActivity.putExtra("type", "favourites");
+                    startActivity(listActivity);
+                    overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
     @Override
     public void onItemClick(int itemId, View view) {
-        Log.d("CREATION", "onNoteClick: Clicked item id " + itemId);
         Intent listActivity = new Intent(getBaseContext(), DetailsActivity.class);
         listActivity.putExtra("id", "" + itemId);
-//        startActivity(listActivity);
 
-                ActivityOptionsCompat activityOptions = ActivityOptionsCompat.makeSceneTransitionAnimation(this,
-                view, "topPicksImageTransition"); //new Pair<>(view, "topPicksImageTransition")
+        ActivityOptionsCompat activityOptions = ActivityOptionsCompat.makeSceneTransitionAnimation(this,
+                view, "topPicksImageTransition");
         ActivityCompat.startActivity(this, listActivity, activityOptions.toBundle());
     }
 
-
-
-    /**
     private void createCategoryClickListeners() {
-        RelativeLayout relativeLayoutWooden = (RelativeLayout) findViewById(R.id.relative_layout_wooden);
-        relativeLayoutWooden.setOnClickListener(new View.OnClickListener() {
+        mainActivityVH.wooden_category_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent woodenIntent = new Intent(getBaseContext(), ListActivity.class);
-                woodenIntent.putExtra("categoryName", "wooden");
-                startActivity(woodenIntent);
+                Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
+                listActivity.putExtra("type", "wooden");
+                startActivity(listActivity);
+                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
             }
         });
 
-        RelativeLayout relativeLayoutMetal = (RelativeLayout) findViewById(R.id.relative_layout_metal);
-        relativeLayoutMetal.setOnClickListener(new View.OnClickListener() {
+        mainActivityVH.metal_category_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent woodenIntent = new Intent(getBaseContext(), ListActivity.class);
-                woodenIntent.putExtra("categoryName", "metallic");
-                startActivity(woodenIntent);
+                Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
+                listActivity.putExtra("type", "metallic");
+                startActivity(listActivity);
+                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
             }
         });
 
-        RelativeLayout relativeLayoutGlass = (RelativeLayout) findViewById(R.id.relative_layout_glass);
-        relativeLayoutGlass.setOnClickListener(new View.OnClickListener() {
+        mainActivityVH.glass_category_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent woodenIntent = new Intent(getBaseContext(), ListActivity.class);
-                woodenIntent.putExtra("categoryName", "glass");
-                startActivity(woodenIntent);
+                Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
+                listActivity.putExtra("type", "glass");
+                startActivity(listActivity);
+                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
             }
         });
 
-        RelativeLayout relativeLayoutHandles = (RelativeLayout) findViewById(R.id.relative_layout_handles);
-        relativeLayoutHandles.setOnClickListener(new View.OnClickListener() {
+        mainActivityVH.handle_category_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent woodenIntent = new Intent(getBaseContext(), ListActivity.class);
-                woodenIntent.putExtra("categoryName", "doorHandle");
-                startActivity(woodenIntent);
+                Intent listActivity = new Intent(getBaseContext(), ListActivity.class);
+                listActivity.putExtra("type", "handle");
+                startActivity(listActivity);
+                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
             }
         });
-    }**/
-
+    }
 }
